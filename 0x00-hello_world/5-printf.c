3beac683de5738e58b1328cd89a43ec80adfd07b
Tajudeen99 <bankkytj@gmail.com>
2022-10-14 04:22:27 +0000

    Hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+  * main - Print Hello
+  *
+  * Return: Always 0 (Successful)
+  */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0)
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdion.h>
+
+/**
+  *main - Print size is not grandeur
+  *
+Return: Always 0 (Sucessful)
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
