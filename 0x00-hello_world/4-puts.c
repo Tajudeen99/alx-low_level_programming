e13e1202b70dd8b3bbb1dd4c5dd5553a35b31c35
Tajudeen99 <bankkytj@gmail.com>
2022-10-15 08:45:36 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@ Return: 0 (if there is no error)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
