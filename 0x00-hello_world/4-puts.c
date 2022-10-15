6a6d16afc3a98d0135d22db4710edd3beb839638
Tajudeen99 <bankkytj@gmail.com>
2022-10-15 09:15:12 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 
 /**
   *main - To print and followed by a new line
@@ -7,6 +7,6 @@ Return: Returns 0, if successful
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n"); //Hint
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
 }
