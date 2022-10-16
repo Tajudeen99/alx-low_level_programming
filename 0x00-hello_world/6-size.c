d098ad16c1dac8429bfde48516192d2a8b90f32e
Tajudeen99 <bankkytj@gmail.com>
2022-10-16 17:33:41 +0000

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	/*Sizes of variable*/
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
