590ca862d2369d0d77a4d8a314845a593e75dce1
Tajudeen99 <bankkytj@gmail.com>
2022-10-16 20:48:15 +0000

    program that prints a string to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,6 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 99);
-	write(1, "\n", 5);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 99);
+	return (1);
 }
