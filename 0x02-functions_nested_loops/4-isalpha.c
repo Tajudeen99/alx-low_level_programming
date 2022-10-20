008288666ec1d55961898d41d3fa7c5b69284fc0
Tajudeen99 <bankkytj@gmail.com>
2022-10-20 04:05:29 +0000

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ * _isalpha - Return 1 if c is a letter. Lowercase or uppercase
  * @c: unsigned int value to be compared with ASCII value
  * Return: 1 if its alphabet otherwise 0
  */
