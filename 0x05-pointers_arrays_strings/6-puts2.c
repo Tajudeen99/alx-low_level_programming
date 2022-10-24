c3a7be0d1383b4fcfd48faef7016facd0ac07808
Tajudeen99 <bankkytj@gmail.com>
2022-10-24 23:45:03 +0000

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - Prints character of a string
+ * @str: pointer parameter
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
